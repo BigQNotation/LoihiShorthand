75b1f5713e844bff424378737671550a1afb0357
Chihin Lau <chihin.lau@wsu.edu>
2019-11-18 16:32:51 -0800

    Can draw on screen now

--- app/src/main/java/com/example/shorthand/WriteActivity.java
+++ app/src/main/java/com/example/shorthand/WriteActivity.java
@@ -1,14 +1,21 @@
 package com.example.shorthand;
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.DisplayMetrics;
+
 
 public class WriteActivity extends AppCompatActivity {
+    private FingerView fingerView;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_write);
+        setContentView(R.layout.activity_draw);
+        fingerView = (FingerView) findViewById(R.id.paintView);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        fingerView.init(metrics);
+        //paintView.normal();
+
     }
 
 
