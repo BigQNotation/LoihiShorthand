041e9c026b5458c6f67f52fc2ba2cd6cf993fd61
donovan.roberts <donovan.roberts@wsu.edu>
2020-03-06 14:06:43 -0800

    Added a way to save graphs as png files in function called saveGraph

--- desktopServer/DesktopApplication.py
+++ desktopServer/DesktopApplication.py
@@ -14,7 +14,8 @@ class DesktopApp:
         self.graphTimer = 20
         self.q = Queue(maxsize=10000)
         self.xPoint, self.yPoint = [], []
-        self.fig, self.myPlt = plt.subplots()
+        self.fig = plt.figure()
+        #self.myPlt = plt.subplots()
         
     #Setup the window to specific size
     def setupWindow(self):
@@ -54,17 +55,26 @@ class DesktopApp:
 
         while(self.q.empty() == False):
             newData = self.q.get()
-            print("newData: ", newData)
-            print("self.xPoint: ", len(self.xPoint))
-            print("self.yPoint: ", len(self.yPoint))
-            self.xPoint.append(float(newData[0]))
-            self.yPoint.append(float(newData[1]))
-            if (self.q.empty()):
-                print("empty q")
+            #print("newData: ", newData)
+            #print("self.xPoint: ", len(self.xPoint))
+            #print("self.yPoint: ", len(self.yPoint))
+            if(float(newData[0]) != -1.0):
+                print("New Data Added")
+                self.xPoint.append(float(newData[0]))
+                self.yPoint.append(float(newData[1]))
+            else:
+                self.saveGraph()
         self.graphStuff()
+
+    def saveGraph(self):
+        print("saving graph")
+        #self.fig = plt.figure()
+        self.fig.savefig('plot.png')
+
 
     #Graphs the informations in the data sets x and y, shows them in a plot.  
     def graphStuff(self):
+        self.fig = plt.gcf()
         plt.clf()
         plt.scatter(self.xPoint, self.yPoint, s=50, marker='o') 
         plt.plot(self.xPoint, self.yPoint) #plot a line graph
