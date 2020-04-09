4ce38100b946f11681417a2cb92eda1e76861f1d
CHL <darkdarkbee@gmail.com>
2020-04-08 20:04:23 -0700

    modified show.py

---
+++ trainingset/2GestureSet/createtraining.py
@@ -0,0 +1,29 @@
+from tkinter import *
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import deque
+from queue import Queue
+import datetime
+
+import csv
+import os
+from tempfile import TemporaryFile
+import glob
+import cv2
+
+# save image and label to numpy binary files
+label_swapper = 0
+image_array = []
+label_array = []
+files = glob.glob('./*.PNG')
+for myFile in files:
+    image = cv2.imread(myFile)
+    dim = 20,20
+    resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+    resized_gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+    image_array.append(resized_gray)
+    label_array.append(label_swapper % 2) # change mod to match amount of gestures to be trained
+    label_swapper = label_swapper + 1
+np.save('./imageTrain',image_array)
+np.save('./labelTrain',label_array)
---
+++ trainingset/2GestureSet/showLabel.py
@@ -0,0 +1,4 @@
+import numpy as np
+labels = np.load('./labelTrain.npy')
+#labels = np.load('./labelInfer.npy')
+print(labels)
