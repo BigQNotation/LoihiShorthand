0c566de7a29947d83ba531605ad2d47e994e632a
CHL <darkdarkbee@gmail.com>
2020-04-10 02:41:05 -0700

    add epl_gestures.py. fix createtraining.py bug

--- trainingset/2GestureSet/infer/createtraining.py
+++ trainingset/2GestureSet/infer/createtraining.py
@@ -17,14 +17,23 @@ label_swapper = 0
 image_array = []
 label_array = []
 #files = glob.glob('./*.PNG')
-files = glob.glob('./*.png')
+files = sorted(glob.glob('./*.png'))
+i=0
 for myFile in files:
     image = cv2.imread(myFile)
-    dim = 20,20
+    print(myFile)
+    #plt.imshow(image, cmap='gray')
+    #plt.show()
+    dim = 10,10
     resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
     resized_gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+    #plt.imshow(resized_gray,cmap='gray' )
+    #plt.show()
     image_array.append(resized_gray)
+    #plt.imshow(image_array[i], cmap='gray')
+    #plt.show()
+    i=i+1
     label_array.append(label_swapper % 2) # change mod to match amount of gestures to be trained
     label_swapper = label_swapper + 1
-np.save('./imageTrain',image_array)
-np.save('./labelTrain',label_array)
+np.save('./imageInfer',image_array)
+np.save('./labelInfer',label_array)
